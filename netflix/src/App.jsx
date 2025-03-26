057f025b3a9478a8b55e46bc9cda7bb041f929cc
Kanishk <kanishksh2608@gmail.com>
2025-03-26 20:12:19 +0530

    Initial project setup

---
+++ netflix/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <div className='bg-zinc-700'>
+       <p className="text-3xl text-white">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
